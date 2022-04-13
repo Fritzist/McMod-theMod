25690f1ed527ddd8bb3674f98d72ebba3016bd63
Fritzist <danielhartl856@gmail.com>
2022-04-13 16:04:11 +0200

    first

---
+++ src/main/java/de/fritzist/theMod/Main.java
@@ -0,0 +1,37 @@
+package de.fritzist.theMod;
+
+import de.fritzist.theMod.item.ModItems;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Main.MODID)
+public class Main {
+
+    public static final String MODID = "themod";
+
+    public Main() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        System.out.println(MODID + ":registering mod");
+
+
+        ModItems.Register(eventBus);
+
+        eventBus.addListener(this::Setup);
+        eventBus.addListener(this::SetupClient);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public void SetupClient(final FMLClientSetupEvent e) {
+    }
+
+    public void Setup(final FMLCommonSetupEvent e) {
+    }
+}
+
---
+++ src/main/java/de/fritzist/theMod/item/ModItems.java
@@ -0,0 +1,36 @@
+package de.fritzist.theMod.item;
+
+import de.fritzist.theMod.Main;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Main.MODID);
+
+    public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+    /*public static final RegistryObject<Item> SILVER_AXE = ITEMS.register("silver_axe",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+    public static final RegistryObject<Item> SILVER_HOE = ITEMS.register("silver_hoe",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+    public static final RegistryObject<Item> SILVER_PICKAXE = ITEMS.register("silver_pickaxe",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+    public static final RegistryObject<Item> SILVER_SHOVEL = ITEMS.register("silver_shovel",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+    public static final RegistryObject<Item> SILVER_SWORD = ITEMS.register("silver_sword",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));*/
+
+
+
+
+
+
+    public static void Register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
